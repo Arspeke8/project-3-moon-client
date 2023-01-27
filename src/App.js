20cbe7a9efa8f57bff2b19380b6ddaf9428ad5ec
Ryan Dong <ryandong@Ryans-MacBook-Pro-2.local>
2023-01-26 18:11:04 -0800

    updated and pushed to Heroku

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Home } from './Home'
 
 // const client = ApolloClient({
 //   uri:'https://my-server-url.heroku.com/graphql',
@@ -9,20 +10,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home/>
     </div>
   );
 }
