acee25acf1fc90dc97c6431037a8ccb8985c16a8
axlrommel <rommelvillagomez77@gmail.com>
2023-02-02 18:35:06 -0600

    folder incorrect

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-
-import logo from "./logo.svg";
-import "./App.css";
-import { Home } from "./pages/Home";
-import { Header } from "./Components/Header";
-import { Footer } from "./Components/Footer";
-import { BrowserRouter } from "react-router-dom";
-import { Route } from "react-router-dom";
-import { Routes } from "react-router-dom";
-import { Login } from "./pages/Login";
-import { Signup } from "./pages/Signup";
-import { Navbar } from "./Components/Navbar";
-import FitnessHistory from "./Components/FitnessHistory";
-import WorkoutForm from "./Components/WorkoutForm";
+import logo from './logo.svg';
+import './App.css';
+import { Home } from './pages/Home';
+import { Header } from './screens/Header';
+import { Footer } from './screens/Footer';
+import { BrowserRouter } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+import { Login } from './pages/Login';
+import { Signup } from './pages/Signup';
+import { Navbar } from './screens/Navbar';
+import FitnessHistory from './screens/FitnessHistory';
+import WorkoutForm from './screens/WorkoutForm';
 
 const Router = BrowserRouter;
-
 
 // const client = ApolloClient({
 //   uri:'https://my-server-url.heroku.com/graphql',
@@ -23,16 +21,16 @@ const Router = BrowserRouter;
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Router>
         <Header />
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="workoutform" element={<WorkoutForm />} />
-          <Route path="fitnesshistory" element={<FitnessHistory />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/Login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='workoutform' element={<WorkoutForm />} />
+          <Route path='fitnesshistory' element={<FitnessHistory />} />
         </Routes>
         <Footer />
       </Router>
